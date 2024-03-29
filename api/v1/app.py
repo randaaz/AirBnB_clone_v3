72939d7a0996ecb0520d9af3a77e2d0669702ca7
rashaaz <138706810+rashaaz@users.noreply.github.com>
2024-03-29 16:30:16 +0300

    modify

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,38 +1,45 @@
 #!/usr/bin/python3
-"""app"""
-from flask import Flask, make_response, jsonify
-from models import storage
-from api.v1.views import app_views
-from os import getenv
+"""
+app
+"""
+
+from flask import Flask, jsonify
 from flask_cors import CORS
+from os import getenv
+
+from api.v1.views import app_views
+from models import storage
 
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
-app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def tear(self):
-    ''' closes storage engine '''
+def teardown(exception):
+    """
+    teardown function
+    """
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
-    ''' handles 404 error and gives json formatted response '''
-    return make_response(jsonify({'error': 'Not found'}), 404)
+def handle_404(exception):
+    """
+    handles 404 error
+    :return: returns 404 json
+    """
+    data = {
+        "error": "Not found"
+    }
 
-if __name__ == '__main__':
-    if getenv("HBNB_API_HOST") is None:
-        HBNB_API_HOST = '0.0.0.0'
-    else:
-        HBNB_API_HOST = getenv("HBNB_API_HOST")
-    if getenv("HBNB_API_PORT") is None:
-        HBNB_API_PORT = 5000
-    else:
-        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
-    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
+    resp = jsonify(data)
+    resp.status_code = 404
+
+    return(resp)
+
+if __name__ == "__main__":
+    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
--- models/city.py
+++ models/city.py
@@ -1,25 +1,39 @@
 #!/usr/bin/python3
-'''
-    Define the class City.
-'''
-from os import getenv
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+"""
+City Class from Models Module
+"""
+import os
 from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
 import models
-from models.state import State
+storage_type = os.environ.get('HBNB_TYPE_STORAGE')
 
 
 class City(BaseModel, Base):
-    '''
-        Define the class City that inherits from BaseModel.
-    '''
-    __tablename__ = "cities"
-    if getenv("HBNB_TYPE_STORAGE") == "db":
+    """City class handles all application cities"""
+    if storage_type == "db":
+        __tablename__ = 'cities'
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship("Place", backref="cities",
-                              cascade="all, delete, delete-orphan")
+        places = relationship('Place', backref='cities', cascade='delete')
     else:
-        state_id = ""
-        name = ""
+        state_id = ''
+        name = ''
+
+    if storage_type != 'db':
+        @property
+        def places(self):
+            """
+            getter for places
+            :return: list of places in that city
+            """
+            all_places = models.storage.all("Place")
+
+            result = []
+
+            for obj in all_places.values():
+                if str(obj.city_id) == str(self.id):
+                    result.append(obj)
+
+            return result
